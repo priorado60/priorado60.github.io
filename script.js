76444b4dbbb87e3212a95f7b336c1b84db343e43
priorado60 <priorado60@demolaybrasil.org.br>
2025-03-19 20:26:06 -0300

    V0 Demotree priorado

--- script.js
+++ script.js
@@ -13,40 +13,40 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
-let index = 0;
+// let index = 0;
 
-function showSlide(n) {
-    let slides = document.querySelector('.slider');
-    let total = document.querySelectorAll('.slider img').length;
-    index = (n + total) % total; // Evita valores fora do intervalo
-    slides.style.transform = `translateX(${-index * 100}%)`; // Ajuste para 100% por imagem
-}
+// function showSlide(n) {
+//     let slides = document.querySelector('.slider');
+//     let total = document.querySelectorAll('.slider img').length;
+//     index = (n + total) % total; // Evita valores fora do intervalo
+//     slides.style.transform = `translateX(${-index * 100}%)`; // Ajuste para 100% por imagem
+// }
 
-function nextSlide() {
-    showSlide(index + 1);
-}
+// function nextSlide() {
+//     showSlide(index + 1);
+// }
 
-function prevSlide() {
-    showSlide(index - 1);
-}
+// function prevSlide() {
+//     showSlide(index - 1);
+// }
 
-// Remova ou comente a linha abaixo para desativar o slider automático
-// setInterval(nextSlide, 3000);
+// // Remova ou comente a linha abaixo para desativar o slider automático
+// // setInterval(nextSlide, 3000);
 
-// Modal para expandir imagem
-function openModal(img) {
-    document.getElementById("modalImage").src = img.src;
-    document.getElementById("imageModal").style.display = "flex";
+// // Modal para expandir imagem
+// function openModal(img) {
+//     document.getElementById("modalImage").src = img.src;
+//     document.getElementById("imageModal").style.display = "flex";
 
-    // Oculta os botões de navegação
-    document.querySelector('.prev').style.display = "none";
-    document.querySelector('.next').style.display = "none";
-}
+//     // Oculta os botões de navegação
+//     document.querySelector('.prev').style.display = "none";
+//     document.querySelector('.next').style.display = "none";
+// }
 
-function closeModal() {
-    document.getElementById("imageModal").style.display = "none";
+// function closeModal() {
+//     document.getElementById("imageModal").style.display = "none";
 
-    // Exibe os botões de navegação novamente
-    document.querySelector('.prev').style.display = "block";
-    document.querySelector('.next').style.display = "block";
-}
+//     // Exibe os botões de navegação novamente
+//     document.querySelector('.prev').style.display = "block";
+//     document.querySelector('.next').style.display = "block";
+// }
